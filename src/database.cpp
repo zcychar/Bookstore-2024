7aa85246688d0823ea7772405621f2115d4f6f9b
zcychar <zhang.77274@gmail.com>
2024-12-09 20:21:38 +0800

    update:添加基础框架

---
+++ src/database.cpp
@@ -0,0 +1,13 @@
+/*
+ *File:database.cpp
+ *-----------------
+ *This file is the complement of the storage database used in bookstore 2024
+ */
+
+#include "database.h"
+
+#include <cstdio>
+
+namespace database {
+
+}  // namespace database
