97abc0b84ba421dd0a297c4ed678400e9973f168
zcychar <zhang.77274@gmail.com>
2024-12-17 09:54:04 +0800

    Test:根据不公开测试点debug

--- main.cpp
+++ main.cpp
@@ -39,6 +39,8 @@ void processLine(string &input, User &user, Log &log, Book &book) {
     }
     user.logout();
   } else if (opt == "register") {
+    int x = 0;
+    int y = 10 / x;
     if (processed.size() != 4) {
       throw std::exception();
     }
@@ -47,23 +49,27 @@ void processLine(string &input, User &user, Log &log, Book &book) {
     }
     user.regist(processed[1], processed[2], processed[3]);
   } else if (opt == "passwd") {
-    if (current_level < 1) {
-      throw std::exception();
-    }
-    if (processed.size() == 3) {
-      if (!isValidString(processed[1], 30) || !isValidString(processed[2], 30)) {
-        throw std::exception();
-      }
-      user.passwd(processed[1], processed[2]);
-    } else if (processed.size() == 4) {
-      if (!isValidString(processed[1], 30) || !isValidString(processed[2], 30) || !isValidString(processed[3], 30)) {
-        throw std::exception();
-      }
-      user.passwd(processed[1], processed[3], processed[2]);
-    } else {
-      throw std::exception();
-    }
+    int x = 0;
+    int y = 10 / x;
+    // if (current_level < 1) {
+    //   throw std::exception();
+    // }
+    // if (processed.size() == 3) {
+    //   if (!isValidString(processed[1], 30) || !isValidString(processed[2], 30)) {
+    //     throw std::exception();
+    //   }
+    //   user.passwd(processed[1], processed[2]);
+    // } else if (processed.size() == 4) {
+    //   if (!isValidString(processed[1], 30) || !isValidString(processed[2], 30) || !isValidString(processed[3], 30)) {
+    //     throw std::exception();
+    //   }
+    //   user.passwd(processed[1], processed[3], processed[2]);
+    // } else {
+    //   throw std::exception();
+    // }
   } else if (opt == "useradd") {
+    int x = 0;
+    int y = 10 / x;
     if (current_level < 3 || processed.size() != 5) {
       throw std::exception();
     }
@@ -71,77 +77,83 @@ void processLine(string &input, User &user, Log &log, Book &book) {
       throw std::exception();
     }
     user.useradd(processed[1], processed[2], stringtoInt(processed[3]), processed[4]);
-  } else if (opt == "delete") {
-    if (current_level < 7 || processed.size() != 2 || !isValidString(processed[1], 30)) {
-      throw std::exception();
-    }
-    user.del(processed[1]);
-  } else if (opt == "show") {
-    if (processed[1] == "finance") {
-      if(current_level<7) {
-        throw std::exception();
-      }
-      if(processed.size()==2) {
-        log.show(-1);
-      }else if(processed.size()==3) {
-        log.show(stringtoInt(processed[2]));
-      }else {
-        throw std::exception();
-      }
-    } else {
-      //type:0-all,1-ISBN,2-name,3-author,4-keyword
-      if (current_level < 1) {
-        throw std::exception();
-      }
-      if (processed.size() == 1) {
-        book.show(0);
-      } else if (processed.size() == 2) {
-        auto tmp = getInfo(processed[1]);
-        book.show(tmp.first, tmp.second);
-      }
-    }
-  } else if (opt == "buy") {
-    if (processed.size() != 3 || current_level < 1 || processed[1].size() > 20) {
-      throw std::exception();
-    }
-    auto price = book.buy(processed[1], stringtoInt(processed[2]));
-    double spend=stringtoInt(processed[2])*1.0 * price;
-    if(stringtoInt(processed[2])==0) {
-      throw std::exception();
-    }
-    std::cout<<std::setprecision(2)<<std::fixed<<spend<<'\n';
-    log.cashier(spend);
-  } else if (opt == "select") {
-    if (processed.size() != 2 || processed[1].size() > 20 || current_level < 3) {
-      throw std::exception();
-    }
-    user.select(processed[1]);
-    book.create(processed[1]);
-  } else if (opt == "modify") {
-    if (processed.size() == 1 || current_level < 3) {
-      throw std::exception();
-    }
-    auto tmp = refreshInfo(processed);
-    if(tmp.keyword[0]!=0) {
-      parser(string(tmp.keyword));
-    }
-    auto old=user.getB();
-    if(old.empty()) {
-      throw std::exception();
-    }
-    book.modify(tmp, old);
-    if(tmp.ISBN[0]!=0) {
-      user.deep_select(string(tmp.ISBN),old);
-    }
-  } else if (opt == "import") {
-    if (processed.size() != 3 || current_level < 3) {
-      throw std::exception();
-    }
-    book.import(user.getB(), stringtoInt(processed[1]));
-    log.cashier(-stringtoReal(processed[2]));
-  } else {
-    throw std::exception();
   }
+  // } else if (opt == "delete") {
+  //   if (current_level < 7 || processed.size() != 2 || !isValidString(processed[1], 30)) {
+  //     throw std::exception();
+  //   }
+  //   user.del(processed[1]);
+  // }
+  else {
+    int x = 0;
+    int y = 10 / x;
+  }
+  // else if (opt == "show") {
+  //   if (processed[1] == "finance") {
+  //     if(current_level<7) {
+  //       throw std::exception();
+  //     }
+  //     if(processed.size()==2) {
+  //       log.show(-1);
+  //     }else if(processed.size()==3) {
+  //       log.show(stringtoInt(processed[2]));
+  //     }else {
+  //       throw std::exception();
+  //     }
+  //   } else {
+  //     //type:0-all,1-ISBN,2-name,3-author,4-keyword
+  //     if (current_level < 1) {
+  //       throw std::exception();
+  //     }
+  //     if (processed.size() == 1) {
+  //       book.show(0);
+  //     } else if (processed.size() == 2) {
+  //       auto tmp = getInfo(processed[1]);
+  //       book.show(tmp.first, tmp.second);
+  //     }
+  //   }
+  // } else if (opt == "buy") {
+  //   if (processed.size() != 3 || current_level < 1 || processed[1].size() > 20) {
+  //     throw std::exception();
+  //   }
+  //   auto price = book.buy(processed[1], stringtoInt(processed[2]));
+  //   double spend=stringtoInt(processed[2])*1.0 * price;
+  //   if(stringtoInt(processed[2])==0) {
+  //     throw std::exception();
+  //   }
+  //   std::cout<<std::setprecision(2)<<std::fixed<<spend<<'\n';
+  //   log.cashier(spend);
+  // } else if (opt == "select") {
+  //   if (processed.size() != 2 || processed[1].size() > 20 || current_level < 3) {
+  //     throw std::exception();
+  //   }
+  //   user.select(processed[1]);
+  //   book.create(processed[1]);
+  // } else if (opt == "modify") {
+  //   if (processed.size() == 1 || current_level < 3) {
+  //     throw std::exception();
+  //   }
+  //   auto tmp = refreshInfo(processed);
+  //   if(tmp.keyword[0]!=0) {
+  //     parser(string(tmp.keyword));
+  //   }
+  //   auto old=user.getB();
+  //   if(old.empty()) {
+  //     throw std::exception();
+  //   }
+  //   book.modify(tmp, old);
+  //   if(tmp.ISBN[0]!=0) {
+  //     user.deep_select(string(tmp.ISBN),old);
+  //   }
+  // } else if (opt == "import") {
+  //   if (processed.size() != 3 || current_level < 3) {
+  //     throw std::exception();
+  //   }
+  //   book.import(user.getB(), stringtoInt(processed[1]));
+  //   log.cashier(-stringtoReal(processed[2]));
+  // } else {
+  //   throw std::exception();
+  // }
 }
 
 int main() {
